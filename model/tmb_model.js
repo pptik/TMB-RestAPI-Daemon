9fc8cb2be9783bd6f98443180def13b676ccb56e
hynra <hynra@yahoo.com>
2018-05-04 22:42:40 +0700

    refactoring, linting

--- model/tmb_model.js
+++ model/tmb_model.js
@@ -1,49 +1,50 @@
-let db = require('../app').db;
-let tmbTracker = db.collection('tb_tmb_tracker');
-let tmbTrackerHistory = db.collection('tb_tmb_tracker_history');
-let ObjectId = require('mongodb').ObjectID;
-let moment=require('moment');
-let id = require('moment/locale/id');
-let dateFormat="YYYY-MM-DD HH:mm:ss";
-exports.insertToTMBTrackerHistory= (query)=> {
-    return new Promise((resolve, reject)=>{
-        let gpstrackerdate=moment(query.gpsdatetime,dateFormat,'id');
+let db = require('../app').db
+let tmbTracker = db.collection('tb_tmb_tracker')
+let tmbTrackerHistory = db.collection('tb_tmb_tracker_history')
+let ObjectId = require('mongodb').ObjectID
+let moment = require('moment')
+let id = require('moment/locale/id')
+let dateFormat = "YYYY-MM-DD HH:mm:ss"
+
+exports.insertToTMBTrackerHistory = (query) => {
+    return new Promise((resolve, reject) => {
+        let gpstrackerdate = moment(query.gpsdatetime, dateFormat, 'id')
         let tmbQuery = {
-            buscode:query.buscode,
-            koridor:query.koridor,
-            course:query.course,
-            gpsdatetime:gpstrackerdate.toDate(),
-            location:{
-                type:"Point",
-                coordinates:[parseFloat(query.longitude), parseFloat(query.latitude)]
+            buscode: query.buscode,
+            koridor: query.koridor,
+            course: query.course,
+            gpsdatetime: gpstrackerdate.toDate(),
+            location: {
+                type: "Point",
+                coordinates: [parseFloat(query.longitude), parseFloat(query.latitude)]
             },
-            created_at:new Date(),
-            updated_at:new Date()
-        };
+            created_at: new Date(),
+            updated_at: new Date()
+        }
         tmbTrackerHistory.insertOne(tmbQuery, function (err, result) {
-            if(err)reject(false);
-            else resolve(true);
-        });
-    });
-};
-exports.updateToTMBTracker= (query)=> {
-    return new Promise((resolve, reject)=>{
-        let gpstrackerdate=moment(query.gpsdatetime,dateFormat,'id');
+            if (err) reject(false)
+            else resolve(true)
+        })
+    })
+}
+exports.updateToTMBTracker = (query) => {
+    return new Promise((resolve, reject) => {
+        let gpstrackerdate = moment(query.gpsdatetime, dateFormat, 'id')
         let tmbQuery = {
-            koridor:query.koridor,
-            course:query.course,
-            gpsdatetime:gpstrackerdate.toDate(),
-            rawgpsdatetime:query.gpsdatetime,
-            location:{
-                type:"Point",
-                coordinates:[parseFloat(query.longitude), parseFloat(query.latitude)]
+            koridor: query.koridor,
+            course: query.course,
+            gpsdatetime: gpstrackerdate.toDate(),
+            rawgpsdatetime: query.gpsdatetime,
+            location: {
+                type: "Point",
+                coordinates: [parseFloat(query.longitude), parseFloat(query.latitude)]
             },
-            created_at:new Date(),
-            updated_at:new Date()
-        };
-        tmbTracker.updateOne({buscode:query.buscode},{$set:tmbQuery},{ upsert: true },function (err,result) {
-            if(err)reject(false);
-            else resolve(true);
-        });
-    });
-};
+            created_at: new Date(),
+            updated_at: new Date()
+        }
+        tmbTracker.updateOne({ buscode: query.buscode }, { $set: tmbQuery }, { upsert: true }, function (err, result) {
+            if (err) reject(false)
+            else resolve(true)
+        })
+    })
+}
